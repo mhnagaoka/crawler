53eb4eaac03cf655532f3e6fcd67131464bac090
Mauricio Nagaoka <mhnagaoka@gmail.com>
2020-07-05 16:04:44 -0300

    Fix unit tests

--- src/scraper.test.js
+++ src/scraper.test.js
@@ -4,45 +4,46 @@ const readFileAsync = promisify(fs.readFile)
 const scrapeLinks = require('./scraper')
 
 it('Scrapes links from single anchor', () => {
-  const result = scrapeLinks('<a href="https://github.com">Github</a>')
-  expect(result).toEqual(['https://github.com'])
+  const scraped = scrapeLinks('<a href="https://github.com">Github</a>')
+  expect(scraped.links).toEqual(['https://github.com'])
 })
 
 it('Scrapes links from multiple anchors', () => {
-  const result = scrapeLinks(`
+  const scraped = scrapeLinks(`
   <a href="https://github.com">Github</a>
   <a href="https://gitlab.com">Gitlab</a>
   `)
-  expect(result).toEqual(['https://github.com', 'https://gitlab.com'])
+  expect(scraped.links).toEqual(['https://github.com', 'https://gitlab.com'])
 })
 
 it('Should ignore anchors with no reference', () => {
-  const result = scrapeLinks(`
+  const scraped = scrapeLinks(`
   <a>Null</a>
   <a href="https://gitlab.com">Gitlab</a>
   `)
-  expect(result).toEqual(['https://gitlab.com'])
+  expect(scraped.links).toEqual(['https://gitlab.com'])
 })
 
 it('Should ignore non-anchor elements', () => {
-  const result = scrapeLinks(`
+  const scraped = scrapeLinks(`
   <link rel="stylesheet" href="/styles/default.css"/>
   <a href="https://gitlab.com">Gitlab</a>
   `)
-  expect(result).toEqual(['https://gitlab.com'])
+  expect(scraped.links).toEqual(['https://gitlab.com'])
 })
 
 it('Should handle nested elements properly', () => {
-  const result = scrapeLinks(`
+  const scraped = scrapeLinks(`
   <a href="https://gitlab.com">Gitlab or <a href="https://github.com">Github</a></a>
   `)
-  expect(result).toEqual(['https://gitlab.com', 'https://github.com'])
+  expect(scraped.links).toEqual(['https://gitlab.com', 'https://github.com'])
 })
 
 it('Scrapes links from a huge html file', async () => {
-  expect.assertions(2)
+  expect.assertions(3)
   const html = await readFileAsync('./test-data/wikipedia-denis-ritchie.mhtml')
-  const result = scrapeLinks(html)
-  expect(result.length).toBeGreaterThan(0)
-  expect(result).toContain('/wiki/Unix')
+  const scraped = scrapeLinks(html)
+  expect(scraped.title).toEqual('Dennis Ritchie - Wikipedia')
+  expect(scraped.links.length).toBeGreaterThan(0)
+  expect(scraped.links).toContain('/wiki/Unix')
 })
