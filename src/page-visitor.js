ffa845b2eb50cd4d272e057278f718ed0c1dc68f
Mauricio Nagaoka <mhnagaoka@gmail.com>
2020-07-05 11:17:04 -0300

    Improve page visitor error handling

--- src/page-visitor.js
+++ src/page-visitor.js
@@ -7,32 +7,57 @@ const createPageVisitor = () => {
   const visited = new Set()
   const pageVisitor = async (count, node) => {
     const current = url.format(new URL(node), { fragment: false })
+    // We've been here before. Abort.
+    if (visited.has(current)) {
+      return []
+    }
+    let response
+    let error
+    const scrapedLinks = []
     try {
-      const response = await axios.get(current, {
+      // Fetch page contents, scrape links
+      response = await axios.get(current, {
         responseType: 'text',
       })
-      const links = scrapeLinks(response.data)
-      visited.add(current)
-      const absoluteLinks = links.reduce((sum, link) => {
+      error = null
+      scrapedLinks.push(...scrapeLinks(response.data))
+    } catch (err) {
+      response = null
+      error = err
+    }
+    const absoluteLinks = scrapedLinks.reduce((sum, link) => {
+      try {
+        // Convert to absolute links
         const absoluteLink = url.format(new URL(link, current), {
           fragment: false,
         })
-        if (visited.has(absoluteLink) || !absoluteLink.startsWith('http')) {
+        // Ignore links already visited and non-page links
+        if (
+          visited.has(absoluteLink) ||
+          current === absoluteLink ||
+          !absoluteLink.startsWith('http')
+        ) {
           return sum
         }
+        // Add to the resulting array
         sum.push(absoluteLink)
-        // console.log(link, absoluteLink.toString())
         return sum
-      }, [])
-      // TODO persist url, absoluteLinks
+      } catch (err) {
+        return sum
+      }
+    }, [])
+    // Dedupe
+    const uniqueLinks = [...new Set(absoluteLinks)]
+    // TODO persist url, uniqueLinks
+    visited.add(current)
+    if (response) {
       console.log(
-        `count=${count} url=${current} status=${response.status} length=${response.headers['content-length']} totalLinks=${links.length} filteredLinks=${absoluteLinks.length}`
+        `count=${count} url=${current} status=${response.status} ${response.statusText} length=${response.headers['content-length']} totalLinks=${scrapedLinks.length} uniqueLinks=${uniqueLinks.length}`
       )
-      return absoluteLinks
-    } catch (err) {
-      console.error(`Error visiting ${current}: ${err.message}`)
-      return []
+    } else {
+      console.error(`count=${count} url=${current} error=${error.message}`)
     }
+    return uniqueLinks
   }
   return pageVisitor
 }
